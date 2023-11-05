1f16bb73e6ebe41e876aaac99cc4b32c77852de8
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2023-11-05 14:39:25 +0200

    second commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,8 +5,6 @@ let formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form  input'),
-  textarea: document.querySelector('.feedback-form textarea'),
 };
 
 refs.form.addEventListener('input', throttle(onInputData, 500));
@@ -15,30 +13,29 @@ refs.form.addEventListener('submit', onFormSubmit);
 populateFeedbackForm();
 
 function onInputData(e) {
-  formData = {
-    email: refs.input.value.trim(),
-    message: refs.textarea.value.trim(),
-  };
+  formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-
-  const { email, message } = e.currentTarget.elements;
-  console.log({ email: email.value.trim(), message: message.value.trim() });
-
-  if (localStorage.getItem(FEEDBACK_KEY)) {
-    localStorage.removeItem(FEEDBACK_KEY);
-  }
+  
+localStorage.removeItem(FEEDBACK_KEY);
+  console.log(formData);
   e.currentTarget.reset();
   formData = {};
 }
 
 function populateFeedbackForm() {
-  let data = localStorage.getItem(FEEDBACK_KEY);
+ 
+  try {
+    let data = localStorage.getItem(FEEDBACK_KEY);
   if (!data) return;
   formData = JSON.parse(data);
-  refs.input.value = formData.email ?? '';
-  refs.textarea.value = formData.message ?? '';
+Object.entries(formData).forEach(([key, val]) => {
+  refs.form.elements[key].value = val;
+});
+} catch (error) { 
+    console.log(error);
+  }
 }
